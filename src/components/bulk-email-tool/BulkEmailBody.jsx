c642c8a786711e4e154d6b8716b65d4b84e1a075
Thomas Tracy <contact@tjtracy.com>
2021-12-17 12:55:28 -0500

    feat: [MB-1620] Add email body editor
    Add email body and subject line to bulk email tool.

--- src/components/bulk-email-tool/BulkEmailBody.jsx
+++ src/components/bulk-email-tool/BulkEmailBody.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from '@edx/paragon';
+
+import TextEditor from './TextEditor';
 
 export default function BulkEmailBody() {
-  return <div />;
+  const editorRef = useRef(null);
+  const onInit = (event, editor) => { editorRef.current = editor; };
+
+  return (
+    <div className="w-100 m-auto p-lg-4 p-2.5">
+      <Form>
+        <Form.Group controlId="emailSubject">
+          <Form.Label>Subject:</Form.Label>
+          <Form.Control className="w-lg-50" />
+        </Form.Group>
+        <Form.Group controlId="emailBody">
+          <Form.Label>Body:</Form.Label>
+          <TextEditor onInit={onInit} />
+        </Form.Group>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
+    </div>
+  );
 }
