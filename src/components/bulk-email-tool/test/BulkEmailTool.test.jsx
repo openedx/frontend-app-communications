a168c90ca22afdb93e45091229901f6a13862bb6
Thomas Tracy <contact@tjtracy.com>
2022-02-03 13:48:52 -0500

    feat: [MICROBA-1688] Add cohorts to recipients (#16)

--- src/components/bulk-email-tool/test/BulkEmailTool.test.jsx
+++ src/components/bulk-email-tool/test/BulkEmailTool.test.jsx
@@ -3,10 +3,11 @@
  */
 import React from 'react';
 import { Factory } from 'rosie';
-import { render, screen } from '../../../setupTest';
+import { render, screen, cleanup } from '../../../setupTest';
 import BulkEmailTool from '../BulkEmailTool';
-import { getCourseHomeCourseMetadata } from '../data/api';
+import { getCourseHomeCourseMetadata, getCohorts } from '../data/api';
 import '../data/__factories__/courseMetadata.factory';
+import '../data/__factories__/cohort.factory';
 
 jest.mock('../text-editor/TextEditor');
 jest.mock('../bulk-email-task-manager/api', () => ({
@@ -16,6 +17,7 @@ jest.mock('../bulk-email-task-manager/api', () => ({
 jest.mock('../data/api', () => ({
   __esModule: true,
   getCourseHomeCourseMetadata: jest.fn(() => {}),
+  getCohorts: jest.fn(() => {}),
 }));
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -25,15 +27,21 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('BulkEmailTool', () => {
+  beforeEach(() => jest.resetModules());
+  afterEach(cleanup);
   test('BulkEmailTool renders properly when given course metadata', async () => {
     const courseMetadata = Factory.build('courseMetadata');
+    const cohorts = { cohorts: [Factory.build('cohort'), Factory.build('cohort')] };
     getCourseHomeCourseMetadata.mockImplementation(() => courseMetadata);
+    getCohorts.mockImplementation(() => cohorts);
     render(<BulkEmailTool />);
     expect(await screen.findByText('Course')).toBeTruthy();
   });
   test('BulkEmailTool renders error page on no staff user', async () => {
     const courseMetadata = Factory.build('courseMetadata', { is_staff: false });
+    const cohorts = { cohorts: [Factory.build('cohort'), Factory.build('cohort')] };
     getCourseHomeCourseMetadata.mockImplementation(() => courseMetadata);
+    getCohorts.mockImplementation(() => cohorts);
     render(<BulkEmailTool />);
     expect(
       await screen.findByText('An unexpected error occurred. Please click the button below to refresh the page.'),
