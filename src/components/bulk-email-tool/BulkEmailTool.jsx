5a20359856c8a8163842fc61128546c312a86ff4
Maxwell Frank <92897870+MaxFrank13@users.noreply.github.com>
2022-06-28 10:04:55 -0400

    WIP - Removing Pending tasks and styling changes - Microba 1824 (#47)
    * refactor: replaced Pending Tasks with alert and made general styling/accesibility updates
    
    * addressed linter flags
    
    * refactor: replaced Pending Tasks with alert and made general styling/accesibility updates
    
    * fixed linting issues again
    
    * refactor: replaced Pending Tasks with alert and made general styling/accesibility updates
    
    Co-authored-by: Maxwell Frank <mfrank@2u.com>

--- src/components/bulk-email-tool/BulkEmailTool.jsx
+++ src/components/bulk-email-tool/BulkEmailTool.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import { ErrorPage } from '@edx/frontend-platform/react';
+import { Container } from '@edx/paragon';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import BulkEmailTaskManager from './bulk-email-task-manager/BulkEmailTaskManager';
 import NavigationTabs from '../navigation-tabs/NavigationTabs';
 import BulkEmailForm from './bulk-email-form';
 import { CourseMetadataContext } from '../page-container/PageContainer';
 import { BulkEmailProvider } from './bulk-email-context';
+import BackToInstructor from '../navigation-tabs/BackToInstructor';
 
 export default function BulkEmailTool() {
   const { courseId } = useParams();
@@ -18,14 +21,24 @@ export default function BulkEmailTool() {
         <div>
           <NavigationTabs courseId={courseId} tabData={courseMetadata.tabs} />
           <BulkEmailProvider>
-            <div>
+            <Container size="md">
+              <BackToInstructor />
+              <div className="row pb-4.5">
+                <h1 className="text-primary-500">
+                  <FormattedMessage
+                    id="bulk.email.send.email.header"
+                    defaultMessage="Send an email"
+                    description="A label for email form"
+                  />
+                </h1>
+              </div>
               <div className="row">
                 <BulkEmailForm courseId={courseId} cohorts={courseMetadata.cohorts} />
               </div>
               <div className="row py-5">
                 <BulkEmailTaskManager courseId={courseId} />
               </div>
-            </div>
+            </Container>
           </BulkEmailProvider>
         </div>
       ) : (
---
+++ src/components/bulk-email-tool/bulk-email-task-manager/BulkEmailPendingTasksAlert.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { getConfig } from '@edx/frontend-platform';
+import { Hyperlink, Alert } from '@edx/paragon';
+import { WarningFilled } from '@edx/paragon/icons';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
+
+export default function BulkEmailPendingTasksAlert() {
+  return (
+    <>
+      <Alert variant="warning" icon={WarningFilled}>
+        <FormattedMessage
+          id="bulk.email.pending.tasks.description.one"
+          defaultMessage="To view all pending tasks, including email, visit&nbsp;"
+        />
+        <Hyperlink
+          destination={`${getConfig().LMS_BASE_URL}/courses/${window.location.pathname.split('/')[2]}/instructor#view-course-info`}
+          target="_blank"
+          showLaunchIcon={false}
+        >
+          <FormattedMessage
+            id="bulk.email.pending.tasks.link"
+            defaultMessage="Course Info"
+          />
+        </Hyperlink>
+        <FormattedMessage
+          id="bulk.email.pending.tasks.description.two"
+          defaultMessage="&nbsp;in the Instructor Dashboard."
+        />
+      </Alert>
+    </>
+  );
+}
