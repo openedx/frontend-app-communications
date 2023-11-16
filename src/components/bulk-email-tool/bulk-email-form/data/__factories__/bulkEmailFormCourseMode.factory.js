a65d932a758da3d2b27f8d8c33d760417e0a217b
Taras Lytvynenko <69678257+Inferato@users.noreply.github.com>
2023-11-16 16:55:41 +0200

    fix: Logical operations moved into a separate variable

--- src/components/bulk-email-tool/bulk-email-form/data/__factories__/bulkEmailFormCourseMode.factory.js
+++ src/components/bulk-email-tool/bulk-email-form/data/__factories__/bulkEmailFormCourseMode.factory.js
@@ -1,5 +1,9 @@
 import { Factory } from 'rosie'; // eslint-disable-line import/no-extraneous-dependencies
 
+/**
+ * Generates an array of course mode objects using Rosie Factory.
+ * @returns {Array<Object>} An array of course mode objects with attributes 'slug' and 'name'.
+ */
 const courseModeFactory = () => {
   const AuditModeFactory = Factory.define('AuditModeFactory')
     .attr('slug', 'audit')
