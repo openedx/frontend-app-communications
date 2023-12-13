c548d4557417c0b9a37fd1652974aca006ad7893
johnvente <jhon.vente@edunext.co>
2023-12-13 13:52:41 -0500

    fix: linter problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,23 +6,20 @@ module.exports = createConfig('eslint', {
   settings: {
     'import/resolver': {
       webpack: {
-        config: [
-          path.resolve(__dirname, 'webpack.dev.config.js'),
-          path.resolve(__dirname, 'webpack.prod.config.js'),
-        ],
+        config: path.resolve(__dirname, 'webpack.dev.config.js'),
       },
     },
   },
   rules: {
     'react/function-component-definition': 'off',
-    'import/no-extraneous-dependencies': [
-      'error', {
-        devDependencies: false,
-        optionalDependencies: false,
-        peerDependencies: false,
-        packageDir: __dirname,
-      },
-    ],
-
+    'import/prefer-default-export': 'off',
   },
+  overrides: [
+    {
+      files: ['plugins/**/*.jsx'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 });
--- src/components/PluggableComponent/index.test.jsx
+++ src/components/PluggableComponent/index.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { render, waitFor, screen } from '@testing-library/react';
 import PluggableComponent from '.';
 
