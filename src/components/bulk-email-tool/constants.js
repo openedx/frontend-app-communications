62debac8587f5cc6ccac1ad528d186713edf56d2
ihor-romaniuk <ihor.romaniuk@raccoongang.com>
2024-09-09 12:23:25 +0200

    fix: refactor the code

---
+++ src/components/bulk-email-tool/constants.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const DEFAULT_RECIPIENTS_GROUPS = {
+  SELF: 'myself',
+  STAFF: 'staff',
+  ALL_LEARNERS: 'learners',
+  VERIFIED: 'track:verified',
+  AUDIT: 'track:audit',
+};
